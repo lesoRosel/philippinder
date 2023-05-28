7b95ef8f921675cca83ad242d40162fbbca9b224
moises <moisesrosel@istitutocorni.it>
2023-05-28 22:37:18 +0200

    fix index and add to profile requests

--- profile.php
+++ profile.php
@@ -13,18 +13,29 @@ if (!$conn) {
     die("Connessione al database fallita");
 }
 
-// Recupera l'ID del profilo dell'utente (sostituisci con la logica appropriata)
-$id_profilo = $_SESSION['id_utente'];
+// Se viene inviato il comando 'rifiuta', esegui la query DELETE
+if (isset($_POST['rifiuta'])) {
+    $id_match = $_POST['id_match'];
+    $query = 'DELETE FROM match WHERE id = $1';
+    $result = pg_query_params($conn, $query, array($id_match));
 
-// Esegui una query per ottenere i dati del profilo, inclusa la foto, nome e data di nascita
+    if (!$result) {
+        echo 'Errore durante l\'eliminazione del match: ' . pg_last_error($conn);
+    }
+}
+
+// Recupera l'ID del profilo dell'utente
+$id_utente = $_SESSION['id_utente'];
+
+// Esegui una query per ottenere i dati del profilo
 $query = "SELECT utente.nome, utente.data_nascita, profilo.biografia, profilo.foto 
           FROM utente
           JOIN profilo ON utente.id_utente = profilo.id_profilo
-          WHERE utente.id_utente = $id_profilo";
-$result = pg_query($conn, $query);
+          WHERE utente.id_utente = $1";
+$result = pg_query_params($conn, $query, array($id_utente));
 
 if ($result) {
-    // Verifica se esiste un record corrispondente all'ID del profilo
+    // Verifica se esiste un record corrispondente all'ID dell'utente
     if (pg_num_rows($result) > 0) {
         $profilo = pg_fetch_assoc($result);
         
@@ -44,9 +55,27 @@ if ($result) {
     exit;
 }
 
+// Esegui una query per ottenere i match dell'utente
+$query_match = "SELECT utente.nome, utente.data_nascita, profilo.foto
+                FROM Match
+                JOIN utente ON Match.id_utente_match = utente.id_utente
+                JOIN profilo ON Match.id_utente_match = profilo.id_profilo
+                WHERE Match.id_utente_loggato = $1";
+$result_match = pg_query_params($conn, $query_match, array($id_utente));
+
+if ($result_match) {
+    // Recupera i record dei match
+    $match = pg_fetch_all($result_match);
+} else {
+    // Errore nella query
+    echo 'Errore nella query: ' . pg_last_error($conn);
+    exit;
+}
+
 // Chiudi la connessione al database
 pg_close($conn);
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -56,6 +85,9 @@ pg_close($conn);
 </head>
 <body>
     <div class="user-profile">
+        <h1>Il tuo profilo</h1>
+        <!-- Mostra le informazioni del profilo dell'utente -->
+        <div class="user-profile">
         <div id="img">
             <?php
                 // Verifica se la foto esiste e stampala
@@ -70,6 +102,31 @@ pg_close($conn);
         <h1><?php echo $nome; ?></h1>
         <p>Data di nascita: <?php echo $dataNascita; ?></p>
         <p>Biografia: <?php echo $biografia; ?></p>
+    </div>
+    <h1>Richieste</h1>
+        <!-- Mostra le informazioni dei match dell'utente -->
+        <?php foreach ($match as $m): ?>
+            <div class="user-match">
+                <div id="img">
+                    <img src="<?php echo $m['foto']; ?>" alt="Foto del profilo">
+                </div>
+                <h2><?php echo $m['nome']; ?></h2>
+                <p>Data di nascita: <?php echo $m['data_nascita']; ?></p>
+
+                <form action="" method="post">
+                    
+                    <input type="date" name="data_incontro">
+                    <input type="submit" value="Pianifica un incontro">
+                    <input type="submit" value="Rifiuta">
+                </form>
+                <script>
+        // Ricarica la pagina al click del bottone "Passa"
+        document.getElementById("Rifiuta").addEventListener("click", function() {
+            location.reload();
+        });
+    </script>  
+            </div>
+        <?php endforeach; ?>
     </div>
 </body>
 </html>
